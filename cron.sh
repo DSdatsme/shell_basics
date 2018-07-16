218e69dd1af4801657b628d709465d785f44e89c
DSdatsme <darsuratwala@gmail.com>
2018-07-16 15:46:31 +0530

    commit

--- cron.sh
+++ cron.sh
@@ -3,10 +3,10 @@
 
 
 #For all weekdays
-0 14 * * 1-5 D:\Training\Shell\file.sh
+0 14 * * 1-5 D: \hello.sh
 
 #every three hours between 8am to 8pm
-0 8-20/3 * * * D:\Training\Shell\file.sh
+0 8-20/3 * * * \hello.sh
 
 #odd days and even month
-0 0 1-31/2 2-12/2 * D:\Training\Shell\file.sh
+0 0 1-31/2 2-12/2 * \hello.sh
---
+++ hello.sh
@@ -0,0 +1,1 @@
+echo "Hellofrom Cron!!" >> myName.txt
